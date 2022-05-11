8906f7022b54e1f8b858f338504b0c4ef65210d1
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-05-12 00:28:22 +0300

    ptk-npm-publısh-button

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
-import { ExampleComponent } from 'khn-button'
+import { DashedButton, DefaultButton,LinkButton, PrimaryButton, TextButton } from 'khn-button'
 import 'khn-button/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div className="container">
+
+    <PrimaryButton type="primary" text="Primary" />
+    <DefaultButton type="default" text="Default" />
+    <DashedButton type="dashed" text="Dashed" />
+    <TextButton type="text" text="Text" />
+    <LinkButton type="link" text="Link" />
+
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const PrimaryButton = ({type,text}) => {
+  return <button className={styles[type]}>{text}</button>
+  
 }
+export const DefaultButton = ({text, type}) => {
+  return <button className={styles[type]}>{text}</button>
+}
+export const DashedButton = ({type,text}) => {
+  return <button className={styles[type]}>{text}</button>
+}
+export const TextButton = ({type,text}) => {
+  return <button className={styles[type]}>{text}</button>
+}
+export const LinkButton = ({type,text}) => {
+  return <button className={styles[type]}>{text}</button>
+
+  
+}
+
